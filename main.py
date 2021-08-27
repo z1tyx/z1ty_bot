1886eccdede22dc2c085d597be8bcee6167501f4
z1ty <z1tyx>
2021-08-28 01:16:12 +0900

    Connecting to discord

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import logging
 import discord
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+client = discord.Client()
+
+client.run(TOKEN)
